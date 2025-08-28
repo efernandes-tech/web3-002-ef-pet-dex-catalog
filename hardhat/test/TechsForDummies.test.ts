ecd6312659a1957663311ea9a1eb07f3cf47d909
Éderson Fernandes <efernandes.tech@gmail.com>
2025-08-27 23:28:15 -0300

    Writing tests in Mocha

--- hardhat/test/TechsForDummies.test.ts
+++ hardhat/test/TechsForDummies.test.ts
@@ -9,6 +9,7 @@ describe('TechsForDummies', function () {
         const TechsForDummies = await hre.ethers.getContractFactory(
             'TechsForDummies',
         );
+
         const techsForDummies = await TechsForDummies.deploy();
 
         return { techsForDummies, owner, otherAccount };
@@ -22,5 +23,76 @@ describe('TechsForDummies', function () {
         const count = await techsForDummies.count();
 
         expect(count).to.equal(0);
+    });
+
+    it('Should add tech', async function () {
+        const { techsForDummies, owner, otherAccount } = await loadFixture(
+            deployFixture,
+        );
+
+        await techsForDummies.addTech({
+            name: 'React',
+            description:
+                'React is a JavaScript library for building user interfaces with components.',
+            adopters: 1000,
+        });
+
+        const count = await techsForDummies.count();
+
+        expect(count).to.equal(1);
+    });
+
+    it('Should edit tech', async function () {
+        const { techsForDummies, owner, otherAccount } = await loadFixture(
+            deployFixture,
+        );
+
+        await techsForDummies.addTech({
+            name: 'React.js',
+            description:
+                'React is a JavaScript library for building user interfaces with components.',
+            adopters: 1000,
+        });
+
+        await techsForDummies.editTech(1, {
+            name: 'React',
+            description:
+                'React is a JavaScript library for building user interfaces with components.',
+            adopters: 2100,
+        });
+
+        const tech = await techsForDummies.techs(1);
+
+        expect(tech.name).to.equal('React');
+    });
+
+    it('Should remove tech', async function () {
+        const { techsForDummies, owner, otherAccount } = await loadFixture(
+            deployFixture,
+        );
+
+        await techsForDummies.addTech({
+            name: 'React.js',
+            description:
+                'React is a JavaScript library for building user interfaces with components.',
+            adopters: 1000,
+        });
+
+        await techsForDummies.removeTech(1);
+
+        const count = await techsForDummies.count();
+        expect(count).to.equal(0);
+    });
+
+    it('Should NOT remove tech', async function () {
+        const { techsForDummies, owner, otherAccount } = await loadFixture(
+            deployFixture,
+        );
+
+        const instance = techsForDummies.connect(otherAccount);
+
+        await expect(instance.removeTech(1)).to.be.revertedWith(
+            "You don't have permission.",
+        );
     });
 });
